732af25989c2ab4e156336ec7a75bc360b1fb7f5
Cristina Bisp <cristinanoemi@hotmail.com>
2025-03-19 13:01:54 -0300

    Incorporacion de localStorage clases y arreglos

--- main.js
+++ main.js
@@ -1,80 +1,146 @@
 
 // Declaración de variables y arreglo globales
-const admin = 'admin';
-const clave = 'admin';
-const productos = ["Aceite", "Refrijerante", "Agua destilada"];
-let carrito = 0;
 
+class Usuario{
+    constructor(nombre,clave,permisos){
+        this.nombre = nombre;
+        this.clave = clave;
+        this.permisos = permisos;
+    }
+   
+}
+//clase producto
+class Producto{
+    constructor(id,nombre,precio){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio
+    }
+    mostrar(){
+        return `id: ${this.id} nombre: ${this.nombre} precio: ${this.precio}`;
+    }
+}
+//Inicializacion de arreglo de productos
+const productos = [new Producto("a1", "Aceite", 1500),
+    new Producto("a2","Refrijerante",  2000), 
+    new Producto( "a3","Agua destilada",  1000)];
+    //inicializacion de arreglo de usuarios
+const usuarios = [new Usuario("admin","admin","todos")];
 
-//Función que permite mostrar por consola la lista de productos
-function mostrarProducto(a){
-    for (let i = 0; i < a.length; i++) {
-       console.log(`${i+1} - ${a[i]} `);
+//funcion agregar producto
+const agregarProd = (productos)=>{
+    let nombre = prompt("ingrese nombre del producto");
+    if (nombre != NaN) {
+          productos.push(new Producto("",nombre,0));
+    } else {
+      console.log("Ingrese nuevamente");
+    } ;
+}
+
+//función validar usuario
+const validarUser =() =>{
+    let userInLocalStorage = localStorage.getItem("nombre");
+    if (userInLocalStorage) {
+        alert(`Hola de nuevo ${userInLocalStorage}`)
+    } else {
+        let newUser = prompt("Es tu primera vez ingresando, ingresa tu nombre...")
+        localStorage.setItem('nombre', newUser);
     };
 }
-//Función suma
-
-const sumar = (a,b) => {return a + b; }
-//Función agregar al carro
-const agregar = ( a ,b ,c ) =>{
-    let resultado = sumar(a,b);
-    console.log(`Ha sumado al carro el producto ${productos[c]} ${b} Total: ${resultado}`);
-    return resultado;
+//Funcion logearse
+const logearse=(usu)=>{
+    validarUser();
+    console.log('usuario admin y clave admin');
+    //let usuario = prompt("Ingrese usuario admin");
+    let usuario = localStorage.getItem('nombre')
+    if(usuario){
+        let usuarioArr = usu.find(usuarioArr => usuarioArr.nombre === usuario);
+        if (usuarioArr) {
+            let contrasena = prompt("Ingrese clave de usuario");
+            if ( contrasena === usuarioArr.clave) {
+            
+               localStorage.setItem('clave', contrasena);
+               return true
+            }
+        } else {
+            console.log("La clave o el usuario son incorrectos");
+            return false;
+        }
+    }
 }
 
+// funcion del menu para agregar un producto y logearse
+const altaProd=(u,p)=>{
+     logearse(u);
+     agregarProd(p);
+}
+//Función que permite mostrar por consola la lista de productos
+function mostrarProducto(a){
+    a.forEach(element => {
+        console.log(element.mostrar());
+    });
+}
+
+//Función agregar al carro
+const salir =() =>{
+    if (confirm("Realmente desea salir?")) {
+        console.log("Gracias por comprar"); 
+        localStorage.clear();
+        return 4;               
+    }else return parseInt(prompt('Ingrese la opción deseada:\n 1:Mostrar productos \n2:Agregar al carrito'));
+}
+
+//Función suma
+const sumar = () => {
+    let a = parseInt(localStorage.getItem('totalCompra'));
+    for (const element of JSON.parse(localStorage.getItem("carrito"))) {
+        a+=parseInt(element.precio);
+    }
+    localStorage.setItem('totalCompra', a);
+ }
+
 //Función que permite sumar al carrito de compras
-const compras = (a) =>{
-   let carro = a;
+const compras = (prod) =>{
+   let arrCarro=[];
+   let product = prod;
     do {
         let seleccion = parseInt(prompt("Ingrese el codigo del producto o presione 9 para salir"));
-        switch (seleccion) {
-        case 1:
-            carro = agregar(carro,9000, seleccion-1);
-            break;
-        case 2:
-            carro = agregar(carro,20000, seleccion-1);
-            break;
-        case 3:
-            carro = agregar(carro,15000, seleccion-1);
-            break;
-        case 9:
-            console.log(`Gracias por su compra el TOTAL es: ${carro}`);
-            break;
-        default:
+         if (seleccion != NaN) {
+            arrCarro.push(product[seleccion]);
+      
+        } else {
             console.log("El código ingresado es incorrecto");
-            break;
         }
     } while (confirm("Desea agregar otro producto?"))
+        localStorage.setItem('carrito', JSON.stringify(arrCarro));
+        sumar();
+        console.log(`Gracias por su compra el TOTAL es: ${localStorage.getItem("totalCompra")}`);
     return carro;
+}
+
+//funcion que inicia el proceso de compra
+const comprar =(arr)=>{
+
+    console.log(`Recuerde abonar su compra $ ${compras(arr)}`);
 }  
 // Función Menú que permite seleccionar las opciones de inicio
 const menu = () =>{
     let option;
+    localStorage.setItem('totalCompra', 0);
     do{
          option = parseInt(prompt('Ingrese la opción deseada:\n 1:Mostrar productos\n 2:Agregar al carrito\n 3: Alta producto\n 4:Salir'));
          switch (option) {
                 case 1:
-                   mostrarProducto(productos);
+                    mostrarProducto(productos);
                    break;
                 case 2:
-                    carrito = compras(carrito);
-                    console.log(`Recuerde abonar su compra $ ${carrito}`);
+                    comprar(productos);
                     break;
                 case 3:
-                    console.log(`usuaruo ${admin} y clave ${admin}`);
-                    if (admin == prompt("Ingrese usuario admin")) {
-                        if (clave == prompt("Ingrese clave")) {
-                            productos.push(prompt("ingrese el nombre del producto"));
-                            console.log(productos);
-                        } ;                      
-                    } else {
-                        alert("La clave o el usuario son incorrectos")
-                    };
+                    altaProd(usuarios,productos);
                     break;     
                 case 4:
-                    if (confirm("Realmente desea salir?")) {
-                        console.log("Gracias por comprar");                  
-                    }else option = parseInt(prompt('Ingrese la opción deseada:\n 1:Mostrar productos \n2:Agregar al carrito'));
+                    option = salir();
                      break;
                  default:
                       console.log("No es una opcion válida");
@@ -84,5 +150,5 @@ const menu = () =>{
 }
 
 //inicio del carrito
-alert("Bienvenido");
+
 menu();
